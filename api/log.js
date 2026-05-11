fe482d0c851004a4b8d0e811a9ab226e6fb21cee
NagashUD <128180370+NagashUD@users.noreply.github.com>
2026-05-11 02:15:11 +0200

    Add files via upload

--- api/log.js
+++ api/log.js
@@ -6,12 +6,17 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+apiKey: "AIzaSyAGrWJoZ7o_QoS1gZgFmYXVW7mLrjtfkH0",
+
+  authDomain: "idborder.firebaseapp.com",
+
+  projectId: "idborder",
+
+  storageBucket: "idborder.firebasestorage.app",
+
+  messagingSenderId: "23032475884",
+
+  appId: "1:23032475884:web:c9141c162d2f0d551cf5c9"
 };
 
 const app = initializeApp(firebaseConfig);
